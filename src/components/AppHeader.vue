ccaaa3d427ed1dfe6318343e858849c0d30ef63e
stefanopaesano <stefanopaesano022@gmail.com>
2024-03-27 15:44:59 +0100

    new header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -22,44 +22,76 @@
     
 
 
-<header class="navbar navbar-expand-lg navbar-light bg-danger">
+<header class="fixed-top">
+    <nav class="navbar navbar-dark bg-danger">
+        <div class="container-fluid">
+            <!-- Logo visibile solo su dispositivi lg e superiori -->
+            <a class="navbar-brand d-lg-block" href="#">
+                <img :src="imglogo[0].img" alt="Logo" height="40">
+            </a>
 
-    <div class="container">
-           <!-- img logo -->
-        <a class="navbar-logo" href="#">
-            <img :src="imglogo[0].img" alt="Logo" height="40">
-        </a>
-
+            <!-- Toggle button visibile solo da md e ms -->
+            <button class="navbar-toggler d-md-block d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
             
-        <div class="navbar-collapse justify-content-end">
-
-               
-            <ul class="navbar-nav">
+            <!-- link visibile su tutti i dispositivi -->
+            <ul class="navbar-nav d-flex d-md-none">
                 <!-- link -->
-
                 <li class="nav-item">
                     <a class="nav-link" href="lavora con noi">Lavora con noi</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="login">Login</a>
                 </li>
-
-                <!-- Aggiungi icone qui -->
-
+            </ul>
+            
+            <!-- link visibile solo su dispositivi lg e superiori -->
+            <ul class="navbar-nav d-lg-flex flex-row ">
+                <!-- link -->
                 <li class="nav-item">
-                    <a class="nav-link" href="#">
-                        <i class="fas fa-search"></i> 
-                    </a>
+                    <a class="nav-link" href="lavora con noi">Lavora con noi</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">
-                        <i class="material-icons"></i> 
-                    </a>
+                    <a class="nav-link" href="login">Login</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <!-- Collapse div -->
+    <div class="collapse bg-danger lg-none" id="navbarToggleExternalContent" data-bs-theme="black">
+        <div class="bg-danger p-4">
+            <h5 class="text-body-emphasis h4">
+                Link
+            </h5>
+
+            <ul class="navbar-nav">
+                <!-- link collapse visbili solo in md ms -->
+                <li class="nav-item">
+                    <a class="nav-link" href="lavora con noi">Lavora con noi</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="login">Login</a>
                 </li>
             </ul>
 
-        </div>
+            <!-- Aggiungi icone qui -->
 
+            <ul class="navbar-nav d-lg-none">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <i class="fa-solid fa-utensils"></i>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <i class="fa-solid fa-pizza-slice"></i>
+                    </a>
+                </li>
+            </ul>
+            
+        </div>
     </div>
 </header>
 
@@ -71,6 +103,7 @@
 
 header {
     font-family: 'Times New Roman', Times, serif;
+    
 }
 
 .navbar-brand img {
