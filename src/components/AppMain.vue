87848c9e18cbd5abd58ef6fb40b4f1cc8a31de71
DomenicoScala <dome.scala12@gmail.com>
2024-03-27 17:00:11 +0100

    MODIFICATO APP MAIN

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -21,17 +21,9 @@
         <!--Cose da sistemare: 
         1) Fare in modo che quando vado nella versione mobile la foto si adatti automaticamente e non si tagli
         2) Centrare tutto il blocco contenente la barra di ricerca -->
-        <section class="background-main-image">
-            <div class="container-fluid">
-                <div class="container">
-                    <div class="search-restaurant">
-                        <h2>Cerca qui i ristoranti della tua zona!</h2>
-                        <form class="d-flex" role="search">
-                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                            <button class="btn btn-outline-success" type="submit">Search</button>
-                        </form>
-                    </div>
-                </div>
+        <section class="jumbotron">
+            <div class="search-box">
+                <h1>sdfsddsfdfdsf</h1>
             </div>
         </section>
         <!--FINE SEZIONE 1-->
@@ -80,31 +72,15 @@
 <style lang="scss" scoped>
 
 //CSS SEZIONE 1
-.background-main-image {
-  position: relative;
-  width: 100%;
-  height: 100vh;
-  background-image: url('https://png.pngtree.com/background/20230621/original/pngtree-rapidly-satisfy-your-hunger-with-3d-fast-food-background-03-picture-image_3910286.jpg');
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-
-  .search-restaurant {
-    position: absolute;
-    bottom: 50px;
-    width: 80%; 
-    margin: auto;
-    background-color: rgba(255, 255, 255, 0.7);
-    padding: 20px;
-    border-radius: 12px;
-  }
+.jumbotron{
+    background-image: url(https://png.pngtree.com/background/20230621/original/pngtree-rapidly-satisfy-your-hunger-with-3d-fast-food-background-03-picture-image_3910286.jpg);
+    height: 700px;
+    background-repeat: no-repeat;
+    background-size:cover;
 }
 
-@media (min-width: 768px) {
-  .search-restaurant {
-    width: 50%; /* Ritornato al 50% per schermi più grandi */
-  }
-}
+
+
 //FINE CSS SEZIONE 1
 
 
