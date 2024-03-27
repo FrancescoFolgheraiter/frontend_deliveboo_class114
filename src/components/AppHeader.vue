02b095124e4c89d7a068fc4e95c4b0640a3583b4
stefanopaesano <stefanopaesano022@gmail.com>
2024-03-27 16:00:14 +0100

    header completato

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,14 +2,14 @@
     export default{
         data(){
             return{
+
                 imglogo: [
                 {
                     title:"img-logo",
                     img:"https://www.filepicker.io/api/file/PrjQ7ZxTQye3aR2Tzt3N",
                 }
             ],
-
-            };
+        };
         },
         methods:{
             
@@ -23,7 +23,9 @@
 
 
 <header class="fixed-top">
+
     <nav class="navbar navbar-dark bg-danger">
+
         <div class="container-fluid">
             <!-- Logo visibile solo su dispositivi lg e superiori -->
             <a class="navbar-brand d-lg-block" href="#">
@@ -31,23 +33,12 @@
             </a>
 
             <!-- Toggle button visibile solo da md e ms -->
-            <button class="navbar-toggler d-md-block d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler d-md-block d-sm-block d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent">
                 <span class="navbar-toggler-icon"></span>
             </button>
             
-            <!-- link visibile su tutti i dispositivi -->
-            <ul class="navbar-nav d-flex d-md-none">
-                <!-- link -->
-                <li class="nav-item">
-                    <a class="nav-link" href="lavora con noi">Lavora con noi</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="login">Login</a>
-                </li>
-            </ul>
-            
             <!-- link visibile solo su dispositivi lg e superiori -->
-            <ul class="navbar-nav d-lg-flex flex-row ">
+            <ul class="navbar-nav d-none d-lg-flex flex-row">
                 <!-- link -->
                 <li class="nav-item">
                     <a class="nav-link" href="lavora con noi">Lavora con noi</a>
@@ -57,7 +48,9 @@
                 </li>
             </ul>
         </div>
+        
     </nav>
+
 
     <!-- Collapse div -->
     <div class="collapse bg-danger lg-none" id="navbarToggleExternalContent" data-bs-theme="black">
