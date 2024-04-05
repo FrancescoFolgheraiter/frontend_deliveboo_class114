075078f35df8f1bb99d00923002b6eba997740c8
stefanopaesano <stefanopaesano022@gmail.com>
2024-04-05 15:09:22 +0200

    implementazione metodo payement

--- router.js
+++ router.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import IndexPage from './src/components/pages/indexPage.vue';
 import ResturantPage from './src/components/pages/ResturantPage.vue';
+import PayementPage from './src/components/pages/PayementPage.vue';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/resturant',
       name: 'resturant.index',
       component: ResturantPage
+    },
+    {
+      path: '/payement',
+      name: 'payement.index',
+      component: PayementPage
     }
   ]
 });
