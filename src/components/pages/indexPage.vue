0839091f2826fd62d43d8d10da8eea4640f30b90
DomenicoScala <dome.scala12@gmail.com>
2024-04-05 13:56:16 +0200

    MODIFICATO SEZIONE WHY CHOOSE

--- src/components/pages/indexPage.vue
+++ src/components/pages/indexPage.vue
@@ -5,24 +5,6 @@ import TestimonialComponent from '../TestimonialComponent.vue';
     export default{
         data(){
             return{
-                whyChoose : [
-                    {
-                        icon: "fa-regular fa-clock fa-xl",
-                        title: "Puntualità",
-                        text: "La nostra app di consegna si distingue per la sua puntualità impeccabile. Ogni ordine viene gestito con precisione e rapidità."
-                    },
-                    {
-                        icon: "fa-regular fa-star fa-xl",
-                        title: "Qualità",
-                        text:  "Oltre alla puntualità, ci impegniamo a offrire la massima qualità selezionando i migliori ristoranti." 
-                    },
-                    {
-                        icon: "fa-regular fa-face-smile fa-xl",
-                        title: "Cordialità",
-                        text: "Il nostro team è noto per la cordialità e la cortesia, assicurando un servizio clienti caloroso e attento a ogni tua esigenza." 
-                    }
-                ],
-
                 faqAccordion: [
                     {
                         h2: "Quali pagamenti accettate sul vostro sito?",
@@ -39,9 +21,9 @@ import TestimonialComponent from '../TestimonialComponent.vue';
                 ],
 
                 carouselWhyChoose: [
-                    "/img/foto1.png",
                     "/img/foto2.png",
-                    "/img/foto3.png"
+                    "/img/foto3.png",
+                    "/img/foto4.png"
                 ]
 
             };
@@ -118,70 +100,40 @@ import TestimonialComponent from '../TestimonialComponent.vue';
 
 
         <!--INIZIO SEZIONE 4 - PERCHE' SCEGLIERCI -->
-        <section>
-            <div class="container">
-                <h2 class="text-center text-white mb-4">
-                    Perché scegliere i nostri servizi
-                </h2>
-                <div class="container d-flex justify-content-center align-items-center mb-3 p-3">
-                    <div class="card border-0 " >
-                        <div class="row g-0">
-                            <div class="col-lg-8 col-12 ">
-
-                                <!--INIZIO CODICE CAROSELLO DI IMMAGINI-->
-                                <div class="wrapper-name h-100 d-flex align-items-center justify-content-center ">
-                                    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-                                        <div class="carousel-indicators">
-                                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                                        </div>
-                                        <div class="carousel-inner">
-                                            <div class="carousel-item active">
-                                                <img src="/img/foto2.png" class="d-block w-100" alt="...">
-                                            </div>
-                                            <div class="carousel-item">
-                                                <img src="/img/foto2.png" class="d-block w-100" alt="...">
-                                            </div>
-                                            <div class="carousel-item">
-                                                <img src="/img/foto3.png" class="d-block w-100" alt="...">
-                                            </div>
-                                        </div>
-                                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                            <span class="visually-hidden">Previous</span>
-                                        </button>
-                                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                            <span class="visually-hidden">Next</span>
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                            <!--FINE CODICE CAROSELLO DI IMMAGINI-->
-                            
-                            
-                            <!--ICONE, TITOLO E TESTO DEL PERCHE' SCEGLIERCI-->
-                            <div class="box-sezione-4 col-lg-4 col-12  ">
-                                <div class="card-body border-0 p-0">
-                                    <div class="row">
-                                        <div class="col-12" v-for="(elem, i) in whyChoose" :key="i">
-                                            <i class="me-2 mt-4 " :class="elem.icon"></i>
-                                            <h5 class="card-title d-inline circular-text">{{elem.title}}</h5> 
-
-                                            <div class="servizi">
-                                                <p class="card-text  mb-md-3">{{elem.text}}</p>
-                                            </div>
-                                            
-                                        </div>
-                                    </div>
+        <section class="mb-4">
+                <div class="container">
+                    <div class="why-choose">
+                        <div class="img-why">
+                            <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+                                <div class="carousel-inner">
+                                    <div class="carousel-item active" v-for="(pics) in carouselWhyChoose">
+                                    <img :src="pics" class="d-block w-100" alt="...">
+                                    </div>                                  
                                 </div>
                             </div>
                         </div>
+
+                        <div class="text-why">
+                            <h3><span>Stay</span> At Home, We Will Provide <span>Good Food</span></h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, minus! Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, nobis! Lorem ipsum dolor sit amet.</p>
+                            <ul>
+                                <li>
+                                    <i class="fa-regular fa-clock fa-xl"></i>
+                                    <p>Puntualità</p>
+                                </li>
+                                <li>
+                                    <i class="fa-regular fa-star fa-xl"></i>
+                                    <p>Qualità</p>
+                                </li>
+                                <li>
+                                    <i class="fa-regular fa-face-smile fa-xl"></i>
+                                    <p>Cordialità</p>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
         <!--FINE SEZIONE 4-->
 
 
@@ -398,31 +350,70 @@ h4{
 
 
 //CSS SEZIONE 4
-.background-color-section-4{
-    background-color: rgb(219, 53, 69);
-    color: red;
-    
-    
+.why-choose{
+    display: flex;
+    align-items: center;
+    border-radius: 10px;
 
-    .wrapper-name{ ////Classe e sottoclasse applicata per il carosello in modo che la foto mi prende il 100% del contenitore
-    max-width: 800px;
+    
+    >*{
+        width: calc(100% / 2);
+        border-radius:10px;
+    }
+
+    .img-why{
         img{
             width: 100%;
+            object-fit: contain;
+            background-color: white;
+            overflow: hidden;
+            border-radius: 10px;
+        }
+
+        .carousel-color{
+            background-color: #f14647;
         }
     }
 
-    .circular-text:hover, .fa-regular:hover { //Hover sulle icone e sugli H5
-        color: yellow;
-        cursor: pointer;
+}
+
+.text-why{
+    background-color: white;
+    h3{
+        font-weight: 600;
+        font-size: 40px;
+        background-color: white;
+    }
+    
+    span{
+        color: #f14647;
+        background-color: white;
     }
 
-    
-}
-.servizi{
-    margin-top: 0;
-    color: black;
-    padding-left: 10px;
-    padding-top: 0px;
+    p{
+        background-color: white;
+    }
+
+    ul{
+        background-color: white;
+        padding: 0;
+        li{
+            background-color: white;
+            
+            i{
+                color: #f14647;
+            }
+
+
+            p{
+                display: inline-block;
+                margin-left: 10px;
+                font-weight: 600;
+                color:#1f272d;
+            }
+        }
+    }
+
 }
 //FINE CSS SEZIONE 4
 
